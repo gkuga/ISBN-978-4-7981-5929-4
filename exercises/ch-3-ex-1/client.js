1f77dc047ffbc167506789bdb286e8c85fc8f31d
Gaku Yasui <gaku.yasui@gmail.com>
2024-07-16 01:07:29 +0900

    3.2.2

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -27,9 +27,9 @@ var authServer = {
  * Add the client information in here
  */
 var client = {
-  "client_id": "",
-  "client_secret": "",
-  "redirect_uris": ["http://localhost:9000/callback"]
+  "client_id": "oauth-client-1",
+  "client_secret": "oauth-client-secret-1",
+  "redirect_uris": ["http://localhost:9000/callback"],
 };
 
 var protectedResource = 'http://localhost:9002/resource';
@@ -44,19 +44,50 @@ app.get('/', function(req, res) {
 });
 
 app.get('/authorize', function(req, res) {
-
   /*
    * Send the user to the authorization server
    */
+  var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+    response_type: 'code',
+    scope: client.scope,
+    client_id: client.client_id,
+    redirect_uri: client.redirect_uris[0],
+  });
 
+  console.log("redirect", authorizeUrl);
+  res.redirect(authorizeUrl);
 });
 
 app.get('/callback', function(req, res) {
-
   /*
    * Parse the response from the authorization server and get a token
    */
+  var code = req.query.code;
+  var form_data = qs.stringify({
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: client.redirect_uris[0]
+  });
+  var headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+  };
+  var tokRes = request('POST', authServer.tokenEndpoint,
+    {
+      body: form_data,
+      headers: headers
+    }
+  );
+  console.log('Requesting access token for code %s', code);
+  if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+    var body = JSON.parse(tokRes.getBody());
 
+    access_token = body.access_token;
+    console.log('Got access token: %s', access_token);
+    res.render('index', { access_token: access_token });
+  } else {
+    res.render('error', { error: 'Unable to fetch access token, server response: ' + tokRes.statusCode })
+  }
 });
 
 app.get('/fetch_resource', function(req, res) {
