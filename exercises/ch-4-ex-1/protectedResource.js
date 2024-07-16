ce9dafff136b085e53ec543b479ff43fa537359c
Gaku Yasui <gaku.yasui@gmail.com>
2024-07-16 12:55:25 +0900

    4.2

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -38,6 +38,19 @@ var getAccessToken = function(req, res, next) {
 
   console.log('Incoming token: %s', inToken);
 
+  nosql.one().make(function(builder) {
+    builder.where('access_token', inToken);
+    builder.callback(function(err, token) {
+      if (token) {
+        console.log("We found a matching token: %s", inToken);
+      } else {
+        console.log('No matching token was found.');
+      };
+      req.access_token = token;
+      next();
+      return;
+    });
+  });
 };
 
 app.options('/resource', cors());
@@ -46,12 +59,16 @@ app.options('/resource', cors());
 /*
  * Add the getAccessToken function to this handler
  */
-app.post("/resource", cors(), function(req, res) {
+app.post("/resource", cors(), getAccessToken, function(req, res) {
 
   /*
    * Check to see if the access token was found or not
    */
-
+  if (req.access_token) {
+    res.json(resource);
+  } else {
+    res.status(401).end();
+  }
 });
 
 var server = app.listen(9002, 'localhost', function() {
