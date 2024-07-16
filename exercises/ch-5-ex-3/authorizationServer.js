39dac78e1c77271853b126d64cccf3add41cde08
Gaku Yasui <gaku.yasui@gmail.com>
2024-07-16 18:23:40 +0900

    5.5

--- exercises/ch-5-ex-3/authorizationServer.js
+++ exercises/ch-5-ex-3/authorizationServer.js
@@ -30,12 +30,11 @@ var clients = [
   {
     "client_id": "oauth-client-1",
     "client_secret": "oauth-client-secret-1",
-    "redirect_uris": ["http://localhost:9000/callback"]
-
+    "redirect_uris": ["http://localhost:9000/callback"],
     /*
      * Add a set of allowed scopes for this client
      */
-
+    "scope": "foo bar",
   }
 ];
 
@@ -54,6 +53,15 @@ app.get('/', function(req, res) {
 app.get("/authorize", function(req, res) {
 
   var client = getClient(req.query.client_id);
+  var rscope = req.query.scope ? req.query.scope.split(' ') : undefined;
+  var cscope = client.scope ? client.scope.split(' ') : undefined;
+
+  if (__.difference(rscope, cscope).length > 0) {
+    var urlParsed = buildUrl(req.query.redirect_uri, {
+      error: 'invalid_scope',
+    });
+    res.redirect(urlParsed); return;
+  }
 
   if (!client) {
     console.log('Unknown client %s', req.query.client_id);
@@ -77,14 +85,17 @@ app.get("/authorize", function(req, res) {
     /*
      * Send the requested scopes to the approval page for rendering
      */
-    res.render('approve', { client: client, reqid: reqid });
+    res.render('approve', {
+      client: client,
+      reqid: reqid,
+      scope: rscope,
+    });
     return;
   }
 
 });
 
 app.post('/approve', function(req, res) {
-
   var reqid = req.body.reqid;
   var query = requests[reqid];
   delete requests[reqid];
@@ -102,7 +113,16 @@ app.post('/approve', function(req, res) {
       /*
        * Make sure the approved scopes from the form are allowed for this client
        */
-
+      var rscope = getScopesFromForm(req.body);
+      var client = getClient(query.client_id);
+      var cscope = client.scope ? client.scope.split(' ') : undefined;
+      if (__.difference(rscope, cscope).length > 0) {
+        var urlParsed = buildUrl(query.redirect_uri, {
+          error: 'invalid_scope',
+        });
+        res.redirect(urlParsed);
+        return;
+      }
 
       var code = randomstring.generate(8);
 
@@ -112,7 +132,10 @@ app.post('/approve', function(req, res) {
        * Save the approved scopes as part of this object
        */
 
-      codes[code] = { request: query };
+      codes[code] = {
+        request: query,
+        scope: rscope,
+      };
 
       var urlParsed = buildUrl(query.redirect_uri, {
         code: code,
@@ -190,8 +213,16 @@ app.post("/token", function(req, res) {
         var access_token = randomstring.generate();
         var refresh_token = randomstring.generate();
 
-        nosql.insert({ access_token: access_token, client_id: clientId });
-        nosql.insert({ refresh_token: refresh_token, client_id: clientId });
+        nosql.insert({
+          access_token: access_token,
+          client_id: clientId,
+          scope: code.scope,
+        });
+        nosql.insert({
+          refresh_token: refresh_token,
+          client_id: clientId,
+          scope: code.scope,
+        });
 
         console.log('Issuing access token %s', access_token);
 
@@ -199,7 +230,12 @@ app.post("/token", function(req, res) {
          * Return scopes as part of the token response
          */
 
-        var token_response = { access_token: access_token, token_type: 'Bearer', refresh_token: refresh_token };
+        var token_response = {
+          access_token: access_token,
+          token_type: 'Bearer',
+          refresh_token: refresh_token,
+          scope: code.scope.join(' ')
+        };
 
         res.status(200).json(token_response);
         console.log('Issued tokens for code %s', req.body.code);
